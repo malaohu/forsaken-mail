8c87ea98eacca98ea40f27132b08f071c18c4756
malaohu <malaohu@live.cn>
2020-05-15 11:01:48 +0800

    fix bug, mailto link

--- public/js/app.js
+++ public/js/app.js
@@ -57,7 +57,7 @@ $(function(){
     localStorage.setItem('shortid', id);
     var mailaddress = id + '@' + location.hostname;
     $('#shortid').val(mailaddress).parent().siblings('button').find('.mail').attr('data-clipboard-text', mailaddress);
-    $("#sendemail").href="mail:" + mailaddress;
+    $("#sendemail").attr("href", "mailto:" + mailaddress);
   };
 
   $('#refreshShortid').click(function() {
