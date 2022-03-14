3e65d447e942a39ba4222fb699487e93de4f1682
jacksonyoyo <sx3255@gmail.com>
2022-03-14 13:48:38 +0800

    更新 mailin.js

--- modules/mailin.js
+++ modules/mailin.js
@@ -5,13 +5,18 @@
 'use strict';
 
 let path = require('path');
-let mailin = require('mailin');
+let mailin = require('node-mailin');
 let config = require(path.join(__dirname, '..', 'config-default.json'));
+let logger = require('tracer').console()
 
 mailin.start(config.mailin);
 
 mailin.on('error', function(err) {
   console.error(err.stack);
+});
+
+mailin.on('message', function (connection, data, content) {
+  logger.info("FROM:" + data.envelopeFrom.address + "  TO:"+ data.envelopeTo[0].address);
 });
 
 module.exports = mailin;
