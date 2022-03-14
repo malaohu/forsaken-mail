3de36ffc77db1d8e183afa0c2436da9036c78c8e
jacksonyoyo <sx3255@gmail.com>
2022-03-14 13:48:30 +0800

    更新 io.js

--- modules/io.js
+++ modules/io.js
@@ -1,26 +1,52 @@
-/**
- * Created by Hongcai Deng on 2015/12/28.
- */
-
 'use strict';
 
 let shortid = require('shortid');
 let mailin = require('./mailin');
-
+let config = require('../config')
 let onlines = new Map();
 
 module.exports = function(io) {
   mailin.on('message', function(connection, data) {
-    let to = data.headers.to.toLowerCase();
+    console.log("*****************************")
+    console.log("标题：" + data.subject)
+    console.log("HTML：" + data.textAsHtml)
+    console.log("文本：" + data.text)
+    console.log("时间：" + data.date)
+    console.log("发件：" + data.from.text)
+    console.log("*****************************")
+      
+    let to = data.envelopeTo[0].address.toLowerCase();
     let exp = /[\w\._\-\+]+@[\w\._\-\+]+/i;
     if(exp.test(to)) {
       let matches = to.match(exp);
       let shortid = matches[0].substring(0, matches[0].indexOf('@'));
       if(onlines.has(shortid)) {
-        onlines.get(shortid).emit('mail', data);
+        let _data = {
+            "subject": data.subject,
+            "text" : data.text,
+            "date" : data.date,
+            "from" : data.from.text,
+            "texthtml" : data.textAsHtml,
+            "html" : data.html
+        }
+        onlines.get(shortid).emit('mail', _data);
       }
     }
   });
+  
+  mailin.on('validateSender', function(session, address, callback) {
+    let hostname = '';
+    if(/@(.*)/.test(address))
+        hostname = RegExp.$1.toLocaleLowerCase()
+
+    if (config.ban_send_from_domain.includes(hostname)) { 
+        let _err = new Error('this domain has  blocked(你的域名已经被我ban了)'); 
+        _err.responseCode = 530; 
+        callback(_err);
+    } else {
+        callback()
+    }   
+  })
 
   io.on('connection', socket => {
     socket.on('request shortid', function() {
@@ -31,6 +57,9 @@ module.exports = function(io) {
     });
 
     socket.on('set shortid', function(id) {
+      if(config.pre_blacklist.indexOf(id.toLowerCase())>-1){
+        return socket.emit('shortid', shortid.generate().toLowerCase());
+      }
       onlines.delete(socket.shortid);
       socket.shortid = id;
       onlines.set(socket.shortid, socket);
